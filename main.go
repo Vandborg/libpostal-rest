cd302aa15e5340ee68d912cacbb6a925f6195d77
Jesper Vandborg <jesper@hungry.dk>
2017-08-25 13:37:57 +0200

    yet again a try for logging

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func HealthHandler(w http.ResponseWriter, r *http.Request) {
 func ExpandHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	fmt.Println(formatRequest(r))
+	fmt.Println("ExpandHandler !!")
 
 	var req Request
 
@@ -83,7 +83,7 @@ func ExpandHandler(w http.ResponseWriter, r *http.Request) {
 func ParserHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	fmt.Println(formatRequest(r))
+	fmt.Println("ParserHandler !!")
 
 	var req Request
 
@@ -93,31 +93,4 @@ func ParserHandler(w http.ResponseWriter, r *http.Request) {
 	parsed := parser.ParseAddress(req.Query)
 	parseThing, _ := json.Marshal(parsed)
 	w.Write(parseThing)
-}
-
-// formatRequest generates ascii representation of a request
-func formatRequest(r *http.Request) string {
- // Create return string
- var request []string
- // Add the request string
- url := fmt.Sprintf(“%v %v %v”, r.Method, r.URL, r.Proto)
- request = append(request, url)
- // Add the host
- request = append(request, fmt.Sprintf(“Host: %v”, r.Host))
- // Loop through headers
- for name, headers := range r.Header {
-   name = strings.ToLower(name)
-   for _, h := range headers {
-     request = append(request, fmt.Sprintf(“%v: %v”, name, h))
-   }
- }
- 
- // If this is a POST, add post data
- if r.Method == “POST” {
-    r.ParseForm()
-    request = append(request, “\n”)
-    request = append(request, r.Form.Encode())
- } 
-  // Return the request as a string
-  return strings.Join(request, “\n”)
 }
